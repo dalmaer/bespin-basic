3f4b25636ff8221d4bc9d7fcd9f65cbffc04e7e9
Ben Galbraith <ben@galbraiths.org>
2010-05-11 17:44:15 -0700

    Removing python backend

--- backend/python/bespin/template/example.js
+++
@@ -1,8 +0,0 @@
-/*
- * This sample code
- * is really lame
- * but we're tired
- */
-function welcome() {
-    return "to Bespin";
-}
--- backend/python/setup.py
+++
@@ -1,8 +0,0 @@
-# Don't run this. This is just for pip.
-
-import pavement
-from setuptools import setup
-
-kw = pavement.options.setup
-
-setup(**kw)
